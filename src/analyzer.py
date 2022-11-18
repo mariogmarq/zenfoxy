a80649e23774d45bc2868dc41b3dc6b3a4ba94c8
mariogmarq <mariogarciamarq@gmail.com>
2022-11-18 22:55:17 +0100

    earliest customer implementation

--- src/analyzer.py
+++ src/analyzer.py
@@ -2,5 +2,20 @@ import pandas as pd
 
 class Analyzer():
     def __init__(self, filepath: str | None = None) -> None:
-        self.df = pd.read_csv(filepath) if filepath != None else None
+        self.df: pd.DataFrame | None = pd.read_csv(filepath) if filepath != None else None
+        if self.df is not None:
+            self.preprocess_data()
     
+    def preprocess_data(self):
+        if self.df is None:
+            return
+        
+        self.df["Last Check-In Date"] = pd.to_datetime(self.df["Last Check-In Date"], format="%d/%m/%Y")
+    
+
+    def get_earliest_customer(self):
+        if self.df is None:
+            return
+
+        sorted = self.df.sort_values(by="Last Check-In Date", ascending=True)
+        return sorted.head(n=1)
