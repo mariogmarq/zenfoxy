8fe98c427e307d0460bc2253227e3abeab857ce8
mariogmarq <mariogarciamarq@gmail.com>
2022-11-18 22:58:18 +0100

    test latest customer

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -4,5 +4,9 @@ import pandas as pd
 def test_earliest_customer():
     analyzer = Analyzer("sample.csv")
     customer: pd.DataFrame = analyzer.get_earliest_customer()
-    print(customer)
     assert customer["First Name"].values[0] == "Anselmo"
+
+def test_latest_customer():
+    analyzer = Analyzer("sample.csv")
+    customer: pd.DataFrame = analyzer.get_latest_customer()
+    assert customer["First Name"].values[0] == "Bjorn"
