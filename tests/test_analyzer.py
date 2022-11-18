00004c19f02a91274fea50ff6b9c2f4d39369de8
mariogmarq <mariogarciamarq@gmail.com>
2022-11-18 23:05:22 +0100

    test list customers

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -10,3 +10,10 @@ def test_latest_customer():
     analyzer = Analyzer("sample.csv")
     customer: pd.DataFrame = analyzer.get_latest_customer()
     assert customer["First Name"].values[0] == "Bjorn"
+
+def test_list_customers():
+    analyzer = Analyzer("sample.csv")
+    list = analyzer.get_customer_list()
+    
+    for i in range(1, len(list)):
+        assert list[i-1] < list[i]
